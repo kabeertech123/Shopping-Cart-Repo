42b83923b40889b17578f097f6197ac2931eb3ac
Kabeer Warsi <kwarsi541@gmail.com>
2026-03-24 21:14:21 +0000

    Add Customer class
    Introduce a simple Customer POJO with name, age, and membership fields and a constructor to initialize them.

---
+++ Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    String name;
+    int age;
+    boolean membership;
+
+    Customer(String name, int age, boolean membership){
+        this.name = name;
+        this.age = age;
+        this.membership = membership;
+    }
+}
